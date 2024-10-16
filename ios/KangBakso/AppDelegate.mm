a461e0e9f2704d35346eb7057c25333ddb820ed7
Schitzos <rizky.rachmawan25@gmail.com>
2024-10-16 23:09:39 +0700

    fix: api key for ios

--- ios/KangBakso/AppDelegate.mm
+++ ios/KangBakso/AppDelegate.mm
@@ -2,13 +2,16 @@
 #import <Firebase.h>
 #import <React/RCTBundleURLProvider.h>
 #import <GoogleMaps/GoogleMaps.h>
+#import "RNCConfig.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   [FIRApp configure];
-  [GMSServices provideAPIKey:@"AIzaSyDP1iZcXX1itkPYZKf2i-MBJuztcVTb0TY"];
+  NSString *googleMapsApiKey = [RNCConfig envFor:@"GOOGLE_MAPS_API_KEY"];
+  [GMSServices provideAPIKey:googleMapsApiKey];
+
   self.moduleName = @"KangBakso";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
